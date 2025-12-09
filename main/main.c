fa0c42aab4d0a39e7ab17e2716ce2dff4a4a2e6d
mattisafurgit <46404761+mattisafur@users.noreply.github.com>
2025-12-09 13:18:19 +0100

    found potential information about magnetometer IC
    https://qstcorp.com/upload/pdf/202202/%EF%BC%88%E5%B7%B2%E4%BC%A0%EF%BC%8913-52-19%20QMC5883P%20Datasheet%20Rev.C(1).pdf
    
    https://forum.arduino.cc/t/are-all-new-gy-270-board-magnetometers-now-fake/1379276/10

--- main/main.c
+++ main/main.c
@@ -9,6 +9,10 @@
 #define MPU_6050_I2C_ADDR 0x68
 #define MPU_6050_I2C_SCL_SPEED_HZ 400000
 
+#define HP_5883_I2C_ADDR 0x2C // NOTE needs validation, might be 0x77
+#define HP_5883_I2C_SCL_SPEED_HZ 400000
+#define HP_5883_I2C_ADDR_LEN 7
+
 void app_main(void)
 {
     esp_err_t err;
@@ -31,9 +35,9 @@ void app_main(void)
     ESP_ERROR_CHECK(err);
     ESP_LOGI(LOG_TAG, "initialized i2c master");
 
-    i2c_master_dev_handle_t i2c_mpu_6050_handle;
-    i2c_device_config_t i2c_mpu_6050_config = {
-        .dev_addr_length = 7, // TODO find value
+    i2c_master_dev_handle_t mpu_6050_i2c_handle;
+    i2c_device_config_t mpu_6050_i2c_config = {
+        .dev_addr_length = 7,
         .device_address = MPU_6050_I2C_ADDR,
         .scl_speed_hz = MPU_6050_I2C_SCL_SPEED_HZ,
         // .scl_wait_us
@@ -41,21 +45,21 @@ void app_main(void)
         //     .disable_ack_check
         // }
     };
-    err = i2c_master_bus_add_device(i2c_master_handle, &i2c_mpu_6050_config, &i2c_mpu_6050_handle);
+    err = i2c_master_bus_add_device(i2c_master_handle, &mpu_6050_i2c_config, &mpu_6050_i2c_handle);
     ESP_ERROR_CHECK(err);
-    ESP_LOGI(LOG_TAG, "initialized i2c device 1");
+    ESP_LOGI(LOG_TAG, "initialized i2c for MPU6050");
 
-    i2c_master_dev_handle_t i2c_device_2_handle;
-    i2c_device_config_t i2c_device_2_config = {
-        .dev_addr_length = 7, // TODO find value
-        .device_address = 0x77,
-        .scl_speed_hz = 100000
+    i2c_master_dev_handle_t hp_5883_i2c_handle;
+    i2c_device_config_t hp_5883_i2c_config = {
+        .dev_addr_length = HP_5883_I2C_ADDR_LEN,
+        .device_address = HP_5883_I2C_ADDR,
+        .scl_speed_hz = HP_5883_I2C_SCL_SPEED_HZ,
         // .scl_wait_us
         // .flags = {
         //     .disable_ack_check
         // }
     };
-    err = i2c_master_bus_add_device(i2c_master_handle, &i2c_device_2_config, &i2c_device_2_handle);
+    err = i2c_master_bus_add_device(i2c_master_handle, &hp_5883_i2c_config, &hp_5883_i2c_handle);
     ESP_ERROR_CHECK(err);
-    ESP_LOGI(LOG_TAG, "initialized i2c device 2");
+    ESP_LOGI(LOG_TAG, "initialized i2c for HP5833");
 }
