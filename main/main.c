a56aa8bc070f56c8b7208306c18e997212a466ba
mattisafurgit <46404761+mattisafur@users.noreply.github.com>
2025-12-09 12:41:34 +0100

    started porting code to accelerometer

--- main/main.c
+++ main/main.c
@@ -1,18 +1,15 @@
-#include <stdlib.h>
-#include <esp_log.h>
 #include <driver/gpio.h>
+#include <driver/i2c.h>
 #include <driver/i2c_master.h>
+#include <esp_log.h>
+#include <stdlib.h>
 
 #define LOG_TAG "MAIN"
-
-#define HW_290_ADDR 0x52
-#define TOF400C_VL53L0X_SCL_FREQ_HZ 400000
 
 void app_main(void)
 {
     esp_err_t err;
 
-    ESP_LOGI(LOG_TAG, "Starting I2C Master Bus Initialization");
     i2c_master_bus_handle_t i2c_master_handle;
     i2c_master_bus_config_t bus_config = {
         .i2c_port = I2C_NUM_0,
@@ -29,19 +26,33 @@ void app_main(void)
     };
     err = i2c_new_master_bus(&bus_config, &i2c_master_handle);
     ESP_ERROR_CHECK(err);
-    ESP_LOGI(LOG_TAG, "I2C Master Bus Initialized Successfully");
+    ESP_LOGI(LOG_TAG, "initialized i2c master");
 
-    ESP_LOGI(LOG_TAG, "Adding I2C Device to Master Bus");
-    i2c_master_dev_handle_t i2c_device_handle;
-    i2c_device_config_t dev_config = {
+    i2c_master_dev_handle_t i2c_device_1_handle;
+    i2c_device_config_t i2c_device_1_config = {
         .dev_addr_length = 7, // TODO find value
-        .device_address = HW_290_ADDR,
-        .scl_speed_hz = TOF400C_VL53L0X_SCL_FREQ_HZ
+        .device_address = 0x68,
+        .scl_speed_hz = 100000
         // .scl_wait_us
         // .flags = {
         //     .disable_ack_check
         // }
     };
-    i2c_master_bus_add_device(i2c_master_handle, &dev_config, &i2c_device_handle);
-    ESP_LOGI(LOG_TAG, "I2C Device Added Successfully");
+    err = i2c_master_bus_add_device(i2c_master_handle, &i2c_device_1_config, &i2c_device_1_handle);
+    ESP_ERROR_CHECK(err);
+    ESP_LOGI(LOG_TAG, "initialized i2c device 1");
+
+    i2c_master_dev_handle_t i2c_device_2_handle;
+    i2c_device_config_t i2c_device_2_config = {
+        .dev_addr_length = 7, // TODO find value
+        .device_address = 0x77,
+        .scl_speed_hz = 100000
+        // .scl_wait_us
+        // .flags = {
+        //     .disable_ack_check
+        // }
+    };
+    err = i2c_master_bus_add_device(i2c_master_handle, &i2c_device_2_config, &i2c_device_2_handle);
+    ESP_ERROR_CHECK(err);
+    ESP_LOGI(LOG_TAG, "initialized i2c device 2");
 }
