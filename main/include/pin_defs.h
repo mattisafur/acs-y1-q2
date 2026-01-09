e2d64e26dae3ca92591905c989ced1f8612beaca
mattisafur <46404761+mattisafur@users.noreply.github.com>
2026-01-09 20:30:32 +0100

    started screating base of code

---
+++ main/include/pin_defs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <driver/gpio.h>
+
+#define PIN_BUZZER GPIO_NUM_12
+
+#define RFID_UART_NUM UART_NUM_1
+#define PIN_RFID_UART_RX GPIO_NUM_32
+#define PIN_RFID_UART_TX GPIO_NUM_39
+#define PIN_RFID_ENABLE GPIO_NUM_33
+
+#define PIN_I2C_SDA GPIO_NUM_21
+#define PIN_I2C_SCL GPIO_NUM_22
