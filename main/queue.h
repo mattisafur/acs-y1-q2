839b9edc6e74fef3ee30e57aa0fcc59e97310074
mattisafur <46404761+mattisafur@users.noreply.github.com>
2026-01-13 13:45:09 +0100

    modified queue message types

--- main/queue.h
+++ main/queue.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <freertos/FreeRTOS.h>
+#include <time.h>
 
 extern QueueHandle_t queue_task_return_handle;
 extern QueueHandle_t queue_card_reader_handle;
@@ -9,20 +10,55 @@ extern QueueHandle_t queue_accelerometer_handle;
 extern QueueHandle_t queue_time_of_flight_handle;
 extern QueueHandle_t queue_metric_handle;
 
-typedef enum queue_task_message_t
+typedef enum message_t
 {
-    QUEUE_MESSAGE_RFID_CARD_VALID,
-    QUEUE_MESSAGE_RFID_CARD_INVALID,
-    QUEUE_MESSAGE_BUZZER_ALARM_START,
-    QUEUE_MESSAGE_BUZZER_ALARM_STOP,
-    QUEUE_MESSAGE_BUZZER_BEEP_CARD_VALID,
-    QUEUE_MESSAGE_BUZZER_BEEP_CARD_INVALID,
-} queue_task_message_t;
+    MESSAGE_ENABLE,
+    MESSAGE_DISABLE,
+    MESSAGE_SENSOR_TRIGGERED,
+    MESSAGE_BUZZER_CARD_VALID,
+    MESSAGE_BUZZER_CARD_INVALID,
+    MESSAGE_CARD_READER_CARD_VALID,
+    MESSAGE_CARD_READER_CARD_INVALID,
+} message_t;
 
-typedef struct queue_metric_message_t
+typedef enum component_t
 {
-    char *task_name;
-    float value;
-} queue_metric_message_t;
+    COMPONENT_BUZZER,
+    COMPONENT_CARD_READER,
+    COMPONENT_ACCELEROMETER,
+    COMPONENT_TIME_OF_FLIGHT,
+} component_t;
+
+typedef struct orchastrator_return_message_t
+{
+    component_t task;
+    message_t message;
+} orchastrator_return_message_t;
+
+typedef enum metric_type_t
+{
+    METRIC_TYPE_ACCELEROMETER_ACCELERATION_X,
+    METRIC_TYPE_ACCELEROMETER_ACCELERATION_Y,
+    METRIC_TYPE_ACCELEROMETER_ACCELERATION_Z,
+    METRIC_TYPE_ACCELEROMETER_ACCELERATION_TOTAL,
+    METRIC_TYPE_ACCELEROMETER_ROTATION_X,
+    METRIC_TYPE_ACCELEROMETER_ROTATION_Y,
+    METRIC_TYPE_ACCELEROMETER_ROTATION_Z,
+    METRIC_TYPE_ACCELEROMETER_ROTATION_TOTAL,
+    METRIC_TYPE_TIME_OF_FLIGHT_DISTANCE,
+    METRIC_TYPE_CARD_READER_VALID,
+} metric_type_t;
+
+typedef struct metric_t
+{
+    metric_type_t metric_type;
+    time_t timestamp;
+    union
+    {
+        float float_value;
+        bool bool_value;
+        uint16_t uint16_value;
+    } value;
+} metric_t;
 
 void queue_init(void);
