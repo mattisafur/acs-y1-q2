137a64503b3eaac5f20d45d80402acce590c0e93
Raygen808 <raygeneugenia21@gmail.com>
2026-01-16 11:50:32 +0100

    time of flight  1.0

--- main/time_of_flight.c
+++ main/time_of_flight.c
@@ -1,3 +1,27 @@
 #include "time_of_flight.h"
 
+#include <driver/gpio.h>
+#include <esp_err.h>
+#include <esp_log.h>
+#include <freertos/queue.h>
+#include <freertos/task.h>
+
+#include "app_config.h"
+#include "queue.h"
+
+#define I2C_NUwM I2C_NUM_0
+#define I2C_SDA GPIO_NUM_21
+#define I2C_SCL GPIO_NUM_22
+
 static const char *TAG = "TIME OF FLIGHT";
+
+static TaskHandle_t time_of_flight_task_handle;
+static QueueHandle_t time_of_flight_queue_handle;
+
+typedef enum time_of_flight_task_queue_message_t
+{
+    TIME_OF_FLIGHT_TASK_QUEUE_MESSAGE_ENABLE,
+    TIME_OF_FLIGHT_TASK_QUEUE_MESSAGE_DISABLE,
+    TIME_OF_FLIGHT_TASK_QUEUE_MESSAGE_TRIGGERD,
+} time_of_flight_task_queue_message_t;
+
